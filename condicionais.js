bfbe39632caf0ba15a10d16c03c12cc0d2e0385f
JadsonPontes <pontes.santos1@outlook.com>
2025-04-03 21:52:04 -0300

    tarefa

---
+++ condicionais.js
@@ -0,0 +1,33 @@
+// let numero = prompt('digite um numero:')
+// if(numero < 9){
+//  console.log("positivo");   
+// }
+
+// else{
+//     if(numero >8){
+//         console.log("negativo");
+//     }
+//     else{
+//         console.log("é 1")
+//     }
+// }
+
+// let numero = prompt("digite ua idade")
+// if(numero >=18){
+//     console.log("maior de idade")
+// }
+// else{
+//     console.log("menor de idade")
+// }
+
+   
+let notaAluno = prompt("Digite a nota do aluno (0 a 100):");
+function verificarAprovacao(nota) {
+    if (nota >= 60) {
+      console.log("Aprovado");
+    } else {
+      console.log("Reprovado");
+    }
+  }
+  
+  
--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-alert("Hello word na tala!");
+// alert("Hello word na tala!");
 
-console.log("Hello word! no console");
+// console.log("Hello word! no console");
+
+let nome = "ariel";
+const idade = 22;
+var cidade = "sao paulo";
+
+// console.log("Meu nome é: " + nome); // Concatenaçao
+// console.log('Meu nome é: ${nome}'); // Interpolaçao
+
+// console.log(" tenho " + idade + " anos ");
+// console.log('Minha cidade é: ' + cidade);
+
+let texto = "tenho 2 cel";
+console.log(texto)
+ 
+console.log(typeof idade)
+
+let n1 = 10;
+let n2 = 10;
+
+let soma = n1 + n2;
+
+console.log(soma);
